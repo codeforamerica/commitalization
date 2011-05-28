0ac45948053e0acbafff8f7e75e7c8ea2c6282e0
Alan Palazzolo <github@zzolo.org>
2011-05-28 01:30:44 -0700

    Still atempting to get data correctly passed around.

--- client/js/client.js
+++ client/js/client.js
@@ -1,19 +1,31 @@
 // Client side JS
 (function($) {
     $(document).ready(function() {
-        var socket = new io.Socket('localhost'); 
+        var socket = new io.Socket(); 
         socket.on('connect', function() {
-            addCommit('connect'); 
+            updateStatus('connected'); 
         }); 
-        socket.on('message', function() {
+        socket.on('message', function(message) {
+            console.log(message);
             addCommit('message');
         });
         socket.on('disconnect', function() {
-            addCommit('disconnect');
+            updateStatus('disconnected');
         });
         
         socket.connect();
-        console.log(socket);
+        
+        
+        //console.log(socket);
+        //socket.send('Hi');
+        
+        $('a.post-test').click(function() {
+            addCommit('testing post....');
+            $.post('committed', getPostData(), function() {
+                addCommit('posted data.');
+            });
+            return false;
+        });
     });
 })(jQuery);
 
@@ -22,4 +34,51 @@
  */
 var addCommit = function(message) {
     $('#commit-container').prepend('<li>' + message + '</li>');
+}
+var updateStatus = function(message) {
+    $('#status-current').html(message);
+}
+
+
+var getPostData = function() {
+    return {
+  "before": "5aef35982fb2d34e9d9d4502f6ede1072793222d",
+  "repository": {
+    "url": "http://github.com/defunkt/github",
+    "name": "github",
+    "description": "You're lookin' at it.",
+    "watchers": 5,
+    "forks": 2,
+    "private": 1,
+    "owner": {
+      "email": "chris@ozmm.org",
+      "name": "defunkt"
+    }
+  },
+  "commits": [
+    {
+      "id": "41a212ee83ca127e3c8cf465891ab7216a705f59",
+      "url": "http://github.com/defunkt/github/commit/41a212ee83ca127e3c8cf465891ab7216a705f59",
+      "author": {
+        "email": "chris@ozmm.org",
+        "name": "Chris Wanstrath"
+      },
+      "message": "okay i give in",
+      "timestamp": "2008-02-15T14:57:17-08:00",
+      "added": ["filepath.rb"]
+    },
+    {
+      "id": "de8251ff97ee194a289832576287d6f8ad74e3d0",
+      "url": "http://github.com/defunkt/github/commit/de8251ff97ee194a289832576287d6f8ad74e3d0",
+      "author": {
+        "email": "chris@ozmm.org",
+        "name": "Chris Wanstrath"
+      },
+      "message": "update pricing a tad",
+      "timestamp": "2008-02-15T14:36:34-08:00"
+    }
+  ],
+  "after": "de8251ff97ee194a289832576287d6f8ad74e3d0",
+  "ref": "refs/heads/master"
+}
 }
--- server/server.js
+++ server/server.js
@@ -2,18 +2,16 @@ var io = require('socket.io');
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var form = require('connect-form');
 
 // Configure express server
 var app = require('express').createServer();
 var templates = path.normalize(__dirname + '/../client');
 app.configure(function() {
     app.use(app.router);
-});
-app.use(express.static(templates));
-
-// Commit post listener
-app.post('/commited', function(req, res) {
-    console.log('posted');
+    app.use(express.bodyParser());
+    app.use(form({ keepExtensions: true }));
+    app.use(express.static(templates));
 });
 
 // Client server
@@ -21,19 +19,48 @@ app.get('/', function(req, res) {
     var t = t || fs.readFileSync(templates + '/client.html', 'utf-8');
     res.send(t);
 });
+
+// Start it up
 app.listen(9955);
 console.log('HTTP Client Listening on http://localhost:9955');
 
 // Socket listener
-var socket = io.listen(app); 
+var socket = io.listen(app);
 socket.on('connection', function(client) {
-console.log('connected');
-
-    client.on('message', function() { 
-        console.log('message'); 
+    console.log('socket connected');
+    client.on('message', function(message) { 
+        console.log('receiving message');
+        console.log(message); 
     }); 
     client.on('disconnect', function() {
-        console.log('disconnect');
-    }); 
-}); 
+        console.log('socket disconnect');
+    });
+    
+    // Commit post listener
+    app.post('/committed', parseForm(), function(req, res) {
+        console.log('posted');
+        client.send(req.params.fields);
+        res.send('');
+    });
+});
 console.log('Server side socket started.');
+
+// Middleware for form handling
+var parseForm = function() {
+    return function(req, res, next) {
+        req.params = req.params || {};
+        if (req.form) {
+            // Do something when parsing is finished
+            // and respond, or respond immediately
+            // and work with the files.
+            req.form.onComplete = function(err, fields, files) {
+                req.params.fields = fields;
+                next();
+            };
+        } else {
+            req.params.fields = req.params.fields || {};
+            next();
+        }
+    
+    }
+};
